ce1688c32f357d3b9d64b03ebe99ca584f36cebb
Muhammed Sahad <muhammedsahad34@gmail.com>
2022-06-30 01:57:48 +0530

    completed registration of mecaf

---
+++ register.php
@@ -0,0 +1,36 @@
+<?php
+  $register_number = $_POST['register_number'];
+  $year = $_POST['year'];
+  $class = $_POST['class'];
+  $event1 = $_POST['event1'];
+  $event2 = $_POST['event2'];
+  $event3 = $_POST['event3'];
+
+  $conn = new mysqli('localhost','root','','signup');
+  if($conn->connect_error){
+    die('connection failed'.$conn->connect_error);
+  }else{ 
+    $stm = $conn->prepare("select * from registration where register_number=?");
+    $stm->bind_param("s",$email);
+    $stm->execute();
+    $user= $stm->get_result();
+    if($user->num_rows > 0){
+        echo ("<script LANGUAGE='JavaScript'>
+    window.alert(' register number already exist');
+    window.location.href='mecaf.html';
+    </script>");
+    }
+    else{
+        $stmt = $conn->prepare("insert into registration(register_number, year, class, event1, event2, event3)values(?, ?, ?, ?, ?, ?)");
+        $stmt->bind_param("sissss", $register_number,$year, $class, $event1, $event2, $event3);
+        $stmt->execute();
+        echo ("<script LANGUAGE='JavaScript'>
+        window.alert('Succesfully registered');
+        window.location.href='innerpage.html';
+        </script>");
+        $stmt->close();
+        $conn->close();
+      }
+
+  }
+    
